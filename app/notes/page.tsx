e15d568004b68dd46a7f9f692ca7415117541a9a
Vadym_Samardak <fatalerror14.88@icloud.com>
2025-08-25 11:29:25 +0200

    curve of studying

---
+++ app/notes/page.tsx
@@ -0,0 +1,19 @@
+// const Notes = () => {
+//   return <div>Notes</div>;
+// };
+
+// export default Notes;
+
+// 1. Імпортуємо функцію
+import { getNotes } from '@/lib/api';
+
+// 2. Робимо фукнцію асинхронною
+const Notes = async () => {
+  // 3. Виконуємо запит
+  const notes = await getNotes();
+  console.log('notes', notes);
+
+  return <div>Notes page</div>;
+};
+
+export default Notes;
---
+++ lib/api.ts
@@ -0,0 +1,25 @@
+// lib/api.ts
+
+import axios from 'axios';
+
+export type Note = {
+  id: string;
+  title: string;
+  content: string;
+  categoryId: string;
+  userId: string;
+  createdAt: string;
+  updatedAt: string;
+};
+
+export type NoteListResponse = {
+  notes: Note[];
+  total: number;
+};
+
+axios.defaults.baseURL = 'https://notehub-public.goit.study/api';
+
+export const getNotes = async () => {
+  const res = await axios.get<NoteListResponse>('/notes');
+  return res.data;
+};
