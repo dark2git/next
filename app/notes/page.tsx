e983b5d976f36dcca737184cd1ba5907e9cb00e4
Vadym_Samardak <fatalerror14.88@icloud.com>
2025-08-26 12:19:30 +0200

    Runtime AxiosError

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -5,15 +5,60 @@
 // export default Notes;
 
 // 1. Імпортуємо функцію
-import { getNotes } from '@/lib/api';
+// import { getNotes } from '@/lib/api';
 
-// 2. Робимо фукнцію асинхронною
-const Notes = async () => {
-  // 3. Виконуємо запит
-  const notes = await getNotes();
-  console.log('notes', notes);
+// // 2. Робимо фукнцію асинхронною
+// const Notes = async () => {
+//   // 3. Виконуємо запит
+//   const notes = await getNotes();
+//   console.log('notes', notes);
 
-  return <div>Notes page</div>;
+//   return <div>Notes page</div>;
+// };
+
+// export default Notes;
+
+// import { getNotes } from '@/lib/api';
+// import NoteList from '@/components/NoteList/NoteList';
+
+// const Notes = async () => {
+//   const response = await getNotes();
+
+//   return (
+//     <section>
+//       <h1>Notes List</h1>
+//       {response?.notes?.length > 0 && <NoteList notes={response.notes} />}
+//     </section>
+//   );
+// };
+
+// export default Notes;
+
+// app/notes/page.tsx
+
+'use client';
+
+import { useState } from 'react';
+import NoteList from '@/components/NoteList/NoteList';
+import { getNotes, Note } from '@/lib/api';
+
+const Notes = () => {
+  const [notes, setNotes] = useState<Note[]>([]);
+
+  const handleClick = async () => {
+    const response = await getNotes();
+    if (response?.notes) {
+      setNotes(response.notes);
+    }
+  };
+
+  return (
+    <section>
+      <h1>Notes List</h1>
+      <button onClick={handleClick}>Get my notes</button>
+      {notes.length > 0 && <NoteList notes={notes} />}
+    </section>
+  );
 };
 
 export default Notes;
